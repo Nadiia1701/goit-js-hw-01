32e27cf807538979aa335bcf03ba27d2dbea9cc8
Nadiia1701 <146233511+Nadiia1701@users.noreply.github.com>
2023-12-08 04:42:59 -0500

    SUmmary

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,11 @@
 "use strict"
 
 function getElementWidth(content, padding, border) {
-    const content2 = Number.parseFloat(content);
-    const padding2 = Number.parseFloat(padding);
-    const border2 = Number.parseFloat(border);
-    return Number.parseFloat(content2 + padding2 * 2 + border2 * 2);
+    const contentWidth = Number.parseFloat(content);
+    const paddingWidth = Number.parseFloat(padding);
+    const borderWidth = Number.parseFloat(border);
+    return Number.parseFloat(contentWidth + paddingWidth * 2 + borderWidth * 2);
 }
-
-getElementWidth();
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
